d9f00828902a9a4700b023e8869b01de7030b50a
Max Base <MaxBaseCode@gmail.com>
2021-11-19 19:57:46 +0330

    Complete the project, run npm install and create package-lock

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+// Elements
+const elm_input = document.querySelector(".input");
+const elm_result = document.querySelector(".result");
+
+// Event
 document.querySelector(".btn").addEventListener("click", () => {
-  //Your Code
+  let count = 0;
+  const length = elm_input.value.length;
+  const chars = elm_input.value.split("");
+  chars.forEach((ch, index) => {
+    if(ch === "ی" && index < length - 1) {
+      if(chars[index + 1] === " ") { // TODO: پشتیبانی از نیم فاصله
+        count += 2;
+      }
+    }
+
+    const num = parseInt(obj[ch]);
+    count += num || 0;
+  });
+  elm_result.innerText = count;
 });
